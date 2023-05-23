4d48452668a992727b9ba48ca460c52102b4034b
davif13 <davi.ffa@gmail.com>
2023-05-23 10:57:08 -0300

    feat: implement bearer token for login

--- src/modules/unauthenticated/screens/Login/index.js
+++ src/modules/unauthenticated/screens/Login/index.js
@@ -1,11 +1,38 @@
-import { Flex, Image } from '@chakra-ui/react'
+import { Flex, Image, useToast } from '@chakra-ui/react'
 import { Text, Input, Link, Button } from 'components'
 import { useNavigate } from 'react-router-dom'
 import { useFormik } from 'formik'
 import * as Yup from 'yup'
+import { useMutation } from 'react-query'
+import { loginCall } from 'services/api/requests'
+import { saveItem } from 'services/storage'
 
 export const LoginScreen = () => {
   const navigate = useNavigate()
+  const toast = useToast()
+
+  const mutation = useMutation((newUser) => loginCall(newUser), {
+    onError: (error) => {
+      toast({
+        title: 'Falha ao realizar login.',
+        description:
+          error?.response?.data?.error || 'Por favor, tente novamente.',
+        status: 'error',
+        duration: 5000,
+        isClosable: true
+      })
+    },
+    onSuccess: (data) => {
+      toast({
+        title: 'Login feito com sucesso.',
+        status: 'success',
+        duration: 6000,
+        isClosable: true
+      })
+      saveItem('@bookclub_token', data?.data?.token)
+      navigate('/')
+    }
+  })
 
   const { handleSubmit, values, handleChange, errors } = useFormik({
     initialValues: {
@@ -20,7 +47,9 @@ export const LoginScreen = () => {
         .min(6, 'Senha deve ter ao menos 6 caracteres.')
         .required('Senha é obrigatória.')
     }),
-    onSubmit: (data) => {}
+    onSubmit: (data) => {
+      mutation.mutate(data)
+    }
   })
 
   return (
@@ -66,7 +95,11 @@ export const LoginScreen = () => {
               Esqueceu sua senha?
             </Link>
           </Flex>
-          <Button onClick={handleSubmit} mt="24px">
+          <Button
+            isLoading={mutation.isLoading}
+            onClick={handleSubmit}
+            mt="24px"
+          >
             Login
           </Button>
           <Link.Action
