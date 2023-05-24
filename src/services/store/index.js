dc559d227871afdf6bd2a396198fb72cf14fe330
davif13 <davi.ffa@gmail.com>
2023-05-24 20:21:47 -0300

    feat: implement global state persistor with redux-persist

--- src/services/store/index.js
+++ src/services/store/index.js
@@ -1,8 +1,38 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import { setupListeners } from '@reduxjs/toolkit/query'
 import userSlice from './slices/user'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+const persistConfig = {
+  key: 'root',
+  storage
+}
+
+const rootReducer = combineReducers({
+  user: userSlice
+})
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
 export const store = configureStore({
-  reducer: {
-    user: userSlice
-  }
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER]
+      }
+    })
 })
+setupListeners(store.dispatch)
+
+export const persistor = persistStore(store)
